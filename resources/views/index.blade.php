2b72a8788618aebb3529dfba4d39462984010505
Pavlína Malinková <pavlina.malinkova@email.cz>
2025-11-30 21:17:43 +0100

    first project for this todo task list in Laravel built according Udemy course

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'Tasks list')
+
+@section('content')
+    <div>
+        @forelse ($tasks as $task)
+            <div><a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a></div>
+        @empty
+            <h2>There are no tasks</h2>
+        @endforelse
+    </div>
+@endsection
